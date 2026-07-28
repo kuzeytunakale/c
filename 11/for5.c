58bea500a6e3e31c70e6aaca439594b757ecd571
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2026-07-28 07:27:57 +0000

    for u anlamak için for ile çarpım tablosu yapıldı

--- 11/for5.c
+++ 11/for5.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main() 
+{
+    int i, number;
+
+    printf("Sayıyı giriniz: ");
+    scanf("%d", &number);
+
+    for(i=1; i<=10; i++)
+    {
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+
+    return 0;
+}
